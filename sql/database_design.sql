5d2f6b04b3c1641d0b1554441444db121d5da359
Moch. Nafi'ul Irsad Al A. <nafiulirsad@gmail.com>
2024-12-09 02:42:31 +0700

    Check and fix

--- sql/database_design.sql
+++ sql/database_design.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    profile_image VARCHAR(255) NULL,
+    profile_image VARCHAR(255) NULL
 );
 
 -- Create banners table
