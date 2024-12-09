dd2edf64d276da54ce5f413dab48fffd045e4015
Moch. Nafi'ul Irsad Al A. <nafiulirsad@gmail.com>
2024-12-09 13:25:27 +0700

    revise, add verifyAuth in /banner

--- app/routes/informationRoutes.js
+++ app/routes/informationRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { verifyAuth } = require('../middlewares/authMiddleware');
 const { getBanners, getServices } = require('../controllers/informationController');
 
-router.get('/banner', getBanners);
+router.get('/banner', verifyAuth, getBanners);
 router.get('/services', verifyAuth, getServices);
 
 module.exports = router;
